66123f665cf4007ac89dbf0a65382507f4274fd2
Infinitifall <42463423+Infinitifall@users.noreply.github.com>
2023-07-03 18:58:58 +1000

    change python to python3

--- run_on_linux.sh
+++ run_on_linux.sh
@@ -2,11 +2,11 @@
 if [ -d "myenv" ]; then
     echo "Found myenv folder."
     source myenv/bin/activate
-    python scripts/run.py
+    python3 scripts/run.py
 else
     echo "myenv folder not found. Creating a new virtual environment."
     python3 -m venv myenv
     source myenv/bin/activate
-    python -m pip install requests
-    python scripts/run.py
+    python3 -m pip install requests
+    python3 scripts/run.py
 fi
