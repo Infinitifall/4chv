ea367b548f543343c62d45739f8e8b46779bea8e
Infinitifall <42463423+Infinitifall@users.noreply.github.com>
2023-12-10 15:24:07 +0530

    restart threads if they die

--- scripts/run.py
+++ scripts/run.py
@@ -21,17 +21,29 @@ def main():
     # delete old html files
     for each_file in pathlib.Path('.').glob('*.html'):
         each_file.unlink()
-
+    
     # create a new thread to download threads
     download_wait_time = 2
     download_thread = threading.Thread(target=download.get_boards_wrapper, args=[download_wait_time])
-    download_thread.start()
 
     # create a new thread to create html pages for the boards
     view_max_threads_per_board = 300
     view_wait_time = 10
     view_thread = threading.Thread(target=view.make_html_wrapper, args=[view_wait_time, view_max_threads_per_board])
-    view_thread.start()
+    
+    # run the threads
+    while True:
+        if not download_thread.is_alive():
+            print("Started download_thread")
+            sys.stdout.flush()
+            download_thread.start()
+        
+        if not view_thread.is_alive():
+            print("Started view_thread")
+            sys.stdout.flush()
+            view_thread.start()
+        
+        time.sleep(60)
 
 
 if __name__ == '__main__':
