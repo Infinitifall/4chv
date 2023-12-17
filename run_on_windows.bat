29548431348971bc90559d54b3edb491caac3a92
Infinitifall <42463423+Infinitifall@users.noreply.github.com>
2023-12-18 03:23:15 +0530

    removed clear, cls, pause, echo instead redirect to /dev/null

--- run_on_windows.bat
+++ run_on_windows.bat
@@ -1,16 +1,10 @@
 @echo off
 if exist myenv (
-    echo Found myenv folder.
     call myenv\Scripts\activate.bat
-    cls
     python scripts\run.py
-    pause
 ) else (
-    echo myenv folder not found. Creating a new virtual environment.
     python -m venv myenv
     call myenv\Scripts\activate.bat
-    python -m pip install requests
-    cls
+    python -m pip install requests >NUL
     python scripts\run.py
-    pause
 )
