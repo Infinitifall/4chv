058519727b96c250571959502e8d74ee0688ba2c
Infinitifall <42463423+Infinitifall@users.noreply.github.com>
2023-12-17 13:27:47 +0530

    added clear, cls and pause

--- run_on_linux.sh
+++ run_on_linux.sh
@@ -2,11 +2,13 @@
 if [ -d "myenv" ]; then
     echo "Found myenv folder."
     source myenv/bin/activate
+    clear
     python3 scripts/run.py
 else
     echo "myenv folder not found. Creating a new virtual environment."
     python3 -m venv myenv
     source myenv/bin/activate
     python3 -m pip install requests
+    clear
     python3 scripts/run.py
 fi
--- run_on_windows.bat
+++ run_on_windows.bat
@@ -2,11 +2,15 @@
 if exist myenv (
     echo Found myenv folder.
     call myenv\Scripts\activate.bat
+    cls
     python scripts\run.py
+    pause
 ) else (
     echo myenv folder not found. Creating a new virtual environment.
     python -m venv myenv
     call myenv\Scripts\activate.bat
     python -m pip install requests
+    cls
     python scripts\run.py
+    pause
 )
